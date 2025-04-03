c9a2f37479109be307defa925969f5f1d7116870
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-04-03 22:09:12 +0500

    Initial commit

---
+++ MySQL.session.sql
@@ -0,0 +1,47 @@
+USE baseballgame;
+
+-- This a table for team members    
+CREATE TABLE Team (
+    TeamID INT AUTO_INCREMENT PRIMARY KEY,
+    TeamName VARCHAR(100) NOT NULL,
+    TeamMascot VARCHAR(100)
+);
+-- this is a player table 
+CREATE TABLE Player (
+    PlayerID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(100) NOT NULL,
+    LastName VARCHAR(100) NOT NULL,
+    Age INT NOT NULL,
+    TeamID INT,
+    FOREIGN KEY (TeamID) REFERENCES Team(TeamID)
+);
+
+
+-- This is a coach table
+CREATE TABLE Coach (
+    CoachID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(100) NOT NULL,
+    LastName VARCHAR(100) NOT NULL,
+    PhoneNumber VARCHAR(15) NOT NULL
+);
+
+
+-- This is a Guardian table
+CREATE TABLE Guardian (
+    GuardianID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(100) NOT NULL,
+    LastName VARCHAR(100) NOT NULL,
+    PhoneNumber VARCHAR(15) NOT NULL,
+    Street VARCHAR(100),
+    City VARCHAR(100),
+    State VARCHAR(100),
+    ZipCode VARCHAR(10),
+    PlayerID INT,
+    FOREIGN KEY (PlayerID) REFERENCES Player(PlayerID)
+);
+
+SHOW TABLES;
+DESCRIBE Team;
+DESCRIBE Player;
+DESCRIBE Coach;
+DESCRIBE Guardian;
